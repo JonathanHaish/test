10e4dccd5d577476e428739089d6961e89e32382
jonatan <jonatanh@sec4cyber.com>
2024-07-08 15:30:52 +0300

    new version

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/",function(req,res){
 // Setup multer for file storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/home/ubuntu/uploads/')
+        cb(null, '/home/ubuntu/myapp/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
